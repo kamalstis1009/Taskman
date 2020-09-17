04e91e3a0501a5f1d011314740d1d97329abb8cf
kamalstis1009@gmail.com <Kamal69@mk.com>
2020-09-17 18:05:00 +0600

    Initial commit

--- app/src/main/java/com/subra/taskman/views/adapters/MeetingAdapter.java
+++ app/src/main/java/com/subra/taskman/views/adapters/MeetingAdapter.java
@@ -3,20 +3,25 @@ package com.subra.taskman.views.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.subra.taskman.R;
+import com.subra.taskman.models.MeetingModel;
+
+import java.util.ArrayList;
 
 public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.MyViewHolder> {
 
     private FragmentActivity mActivity;
-    private int [] arr = {1, 2, 3, 4, 5, 6, 7};
+    ArrayList<MeetingModel> mArrayList;
 
-    public MeetingAdapter(FragmentActivity mActivity) {
+    public MeetingAdapter(FragmentActivity mActivity, ArrayList<MeetingModel> arrayList) {
         this.mActivity = mActivity;
+        this.mArrayList = arrayList;
     }
 
     @NonNull
@@ -28,16 +33,24 @@ public class MeetingAdapter extends RecyclerView.Adapter<MeetingAdapter.MyViewHo
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        MeetingModel model = mArrayList.get(position);
+        holder.title.setText(model.getTitle());
+        holder.remarks.setText(model.getRemarks());
+        holder.date.setText(model.getDate());
     }
 
     @Override
     public int getItemCount() {
-        return arr.length;
+        return mArrayList.size();
     }
 
     class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView title, remarks, date;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = (TextView) itemView.findViewById(R.id.meeting_title);
+            remarks = (TextView) itemView.findViewById(R.id.meeting_remarks);
+            date = (TextView) itemView.findViewById(R.id.meeting_date);
         }
     }
 
